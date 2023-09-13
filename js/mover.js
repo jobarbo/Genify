67a538613bfcf52b32d045ccae7a4213079bf34a
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-09-13 17:46:14 -0400

    fixed mover

--- js/mover.js
+++ js/mover.js
@@ -3,15 +3,15 @@ class Mover {
 		this.x = x;
 		this.y = y;
 		this.initHue = hue;
-		this.initSat = random([0, 20, 40, 60, 80, 100]);
-		this.initBri = random([0, 10, 10, 20, 20, 40, 60, 70, 90]);
+		this.initSat = random([0, 10, 30, 40, 40, 60, 80, 80, 90, 100]);
+		this.initBri = random([0, 10, 20, 20, 40, 40, 60, 70, 80, 90, 100]);
 		this.initAlpha = 100;
-		this.initS = 0.65 * MULTIPLIER;
+		this.initS = 0.35 * MULTIPLIER;
 		this.hue = this.initHue;
 		this.sat = this.initSat;
 		this.bri = this.initBri;
 		this.a = this.initAlpha;
-		this.hueStep = 16;
+		this.hueStep = 2;
 		this.s = this.initS;
 		this.scl1 = scl1;
 		this.scl2 = scl2;
@@ -30,44 +30,32 @@ class Mover {
 		this.oct = 1;
 		this.centerX = width / 2;
 		this.centerY = height / 2;
-		this.borderX = width / 3.5;
+		this.borderX = width / 2;
 		this.borderY = height / 2.75;
-		this.uvalue = 5;
+		this.uvalue = 4;
+		this.isBordered = true;
 	}
 
 	show() {
 		fill(this.hue, this.sat, this.bri, this.a);
 		noStroke();
-		rect(this.x, this.y, this.s);
+		rect(this.x, this.y, this.s, this.s);
 	}
 
 	move() {
 		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct);
 
-		/* 		this.xRandDivider = random([0.1, 30, 50, 100]);
-		this.yRandDivider = random([0.1, 30, 50, 100]); */
-		this.xRandDivider = 0.1;
-		this.yRandDivider = 0.1;
-		/* this.xRandDivider = random(0.01, 12);
-		this.yRandDivider = random(0.01, 12); */
 		this.xRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
 		this.yRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
 
-		//shortand for if this.x is less than 0, set this.x to width and vice versa
 		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
 		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
 
-		//let pxy = p.x - p.y;
-
-		//this.a = map(p.x, -4, 4, this.initAlpha - 5, this.initAlpha + 5, true);
-		//this.s = map(p.x, -24, 24, this.initS + 10, this.initS - 10, true);
 		let pxy = p.x - p.y;
 		this.hue += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, -this.hueStep, this.hueStep, true);
 		this.hue = this.hue > 360 ? this.hue - 360 : this.hue < 0 ? this.hue + 360 : this.hue;
-		//this.sat = map(p.x, -2, 2, 0, 20, true);
-		//this.bri = map(p.x, -2, 2, 0, 40, true);
 
-		this.x =
+		/* 		this.x =
 			this.x <= this.centerX - this.borderX
 				? this.centerX + this.borderX + random(-4 * MULTIPLIER, 0)
 				: this.x >= this.centerX + this.borderX
@@ -78,10 +66,28 @@ class Mover {
 				? this.centerY + this.borderY + random(-4 * MULTIPLIER, 0)
 				: this.y >= this.centerY + this.borderY
 				? this.centerY - this.borderY + random(0, 4 * MULTIPLIER)
-				: this.y;
+				: this.y; */
+
+		/* if (this.isBordered) {
+			if (this.x < (this.xMin - 0.015) * width) {
+				this.x = (this.xMax + 0.015) * width;
+				this.a = 0;
+			}
+			if (this.x > (this.xMax + 0.015) * width) {
+				this.x = (this.xMin - 0.015) * width;
+				this.a = 0;
+			}
+			if (this.y < (this.yMin - 0.015) * height) {
+				this.y = (this.yMax + 0.015) * height;
+				this.a = 0;
+			}
+			if (this.y > (this.yMax + 0.015) * height) {
+				this.y = (this.yMin - 0.015) * height;
+				this.a = 0;
+			}
+		} */
 	}
 }
-
 function superCurve(x, y, scl1, scl2, ang1, ang2, octave) {
 	let nx = x,
 		ny = y,
